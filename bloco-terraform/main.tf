1187fbcd460e865a16e2fe2675f99196b394fcb2
Gilson Silva <gilsonsilvati@gmail.com>
2024-04-01 12:00:46 -0300

    ci: Estrutura de blocos da linguagem HCL (HashiCorp Configuration Language)

---
+++ bloco-terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  
+}
+
+provider "aws" {
+  
+}
+
+resource "aws_instance" "vm1" {
+  
+}
+
+data "aws_ami" "image" {
+  
+}
+
+module "network" {
+  source = "todo"
+}
+
+variable "vm_name" {
+  
+}
+
+output "vm1_ip" {
+  value = "todo"
+}
+
+locals {
+  
+}
